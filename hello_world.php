c1ac2992a7f2fab7170c5a04c68feab5dd5d2e50
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-11 12:21:40 +0300

    For loop added

--- hello_world.php
+++ hello_world.php
@@ -11,7 +11,7 @@
 
 <?php
 	
-	$age = 5;
+	$age = 3;
 	
 	// kui on väiksem kui 18, siis alaealine
 	// kui on suurem siis täisealine
@@ -25,3 +25,21 @@
 	}
 	
 ?>
+
+<br>
+
+<?php
+
+	// nii mitu korda trüki välja "palju ", kui palju on vanust
+	
+	for($i = 0; $i < $age; $i = $i + 1) {
+		
+		// see mida korratakse
+		echo "palju".$i." ";
+		
+	}
+	
+	echo "õnne";
+
+?>
+
