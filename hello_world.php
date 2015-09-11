4c1a119e198199b210ea0c493321eb83dd76168b
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-11 11:36:29 +0300

    Logic added

--- hello_world.php
+++ hello_world.php
@@ -6,3 +6,22 @@
 	echo $first_name." ".$last_name;
 	
 ?>
+
+<br>
+
+<?php
+	
+	$age = 5;
+	
+	// kui on väiksem kui 18, siis alaealine
+	// kui on suurem siis täisealine
+	if($age < 18) {
+		//tõene
+		echo "alaealine";
+		
+	} else {
+		// väär
+		echo "täisealine";
+	}
+	
+?>
