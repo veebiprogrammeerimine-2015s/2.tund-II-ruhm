b19a0beff17f1496dd2bb7f49774c856fab896b2
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-11 13:13:33 +0300

    login form + email validation

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+	
+	//echo $_POST["email"];
+	
+	//Defineerime muutujad
+	$email_error = "";
+	$password_error = "";
+	
+	//kontrollin kas keegi vajutas nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//echo "jah";
+		
+		// kas e-post on tühi
+		if( empty($_POST["email"]) ) {
+			
+			// jah oli tühi
+			$email_error = "See väli on kohustuslik";
+			
+		}
+		
+	}
+
+?>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+	
+	<h2>Login</h2>
+	<form action="login.php" method="post">
+		<input name="email" type="email" placeholder="E-post" > <?php echo $email_error; ?><br><br>
+		<input name="password" type="password" placeholder="Parool" > <br><br>
+		<input type="submit" value="Logi sisse" > <br><br>
+	</form>
+	
+	
+	<h2>Create user</h2>
+	
+</body>
+</html>
